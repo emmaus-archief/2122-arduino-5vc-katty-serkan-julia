61a3a20669253d655f74c132aeb6f9fa3a7491d2
sk1700 <74548162+sk1700@users.noreply.github.com>
2022-04-15 10:10:55 +0200

    Add files via upload

---
+++ stap 2 test code/Ultrasionc-Obstacle-Avoidance.ino
@@ -0,0 +1,129 @@
+/* WaveShare ARPICAR Run Forward/Backward/Left/Right Test
+   
+   ARPICAR run forward,backward,left right and so on..
+   
+   Created 25th June 2016
+           by Xinwu Lin
+           
+   CN: http://www.waveshare.net/
+   EN: http://www.waveshare.com/
+*/
+
+#define PWMA   6           //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
+#define ECHO   2
+#define TRIG   3
+
+int Distance = 0;
+int Speed = 120;
+
+int Distance_test();
+void forward();
+void backward();
+void right();
+void left();
+void stop();
+
+void setup()
+{
+  Serial.begin(115200);  
+  Serial.println("Ultrasionc Obstacle Avoidance example");
+  pinMode(ECHO, INPUT);    // Define the ultrasonic echo input pin
+  pinMode(TRIG, OUTPUT);   // Define the ultrasonic trigger input pin 
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT); 
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  stop();    
+}
+
+void loop()
+{
+  Distance = Distance_test();                 //display distance 
+  Serial.print("Distance = ");            //print distance
+  Serial.print(Distance);       
+  Serial.println("cm");  
+  if(Distance < 25)      //Ultrasonic range ranging 2cm to 400cm
+  {
+    right();
+    //left();
+    delay(500);
+    stop();     
+  }
+  else
+  {
+    forward();    
+  }
+  delay(250);
+}
+
+int Distance_test()         // Measure the distance 
+{
+  digitalWrite(TRIG, LOW);   // set trig pin low 2μs
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);  // set trig pin 10μs , at last 10us 
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);    // set trig pin low
+  float Fdistance = pulseIn(ECHO, HIGH);  // Read echo pin high level time(us)
+  Fdistance= Fdistance/58;       //Y m=（X s*344）/2
+  // X s=（ 2*Y m）/344 ==》X s=0.0058*Y m ==》cm = us /58       
+  return (int)Fdistance;
+}  
+
+void forward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,LOW);  
+  digitalWrite(BIN2,HIGH); 
+}
+
+void backward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void right()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void left()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);  
+}
+
+void stop()
+{
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,LOW);  
+}
