989ff4f0bc11f4d648f58ef419c5e7c8920575ca
JuliaSuijker <74536460+JuliaSuijker@users.noreply.github.com>
2022-05-18 09:50:52 +0200

    Add files via upload

---
+++ stap 2 test code/Joystick.ino
@@ -0,0 +1,144 @@
+#include <Wire.h>
+
+#define PWMA   6           //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
+
+#define Addr  0x20
+
+#define beep_on  PCF8574Write(0xDF & PCF8574Read())
+#define beep_off PCF8574Write(0x20 | PCF8574Read())
+
+byte value;
+int Speed = 150;
+void PCF8574Write(byte data);
+byte PCF8574Read();
+void forward();
+void backward();
+void right();
+void left();
+void stop();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Joystick example!!");
+  Wire.begin();
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT);  
+  analogWrite(PWMA,150);
+  analogWrite(PWMB,150);
+  stop(); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PCF8574Write(0x1F | PCF8574Read());
+  value = PCF8574Read() | 0xE0;
+  if(value != 0xFF)
+  {
+    beep_on;
+    switch(value)
+    { 
+      case 0xFE:
+        forward();
+        Serial.println("up");break; 
+      case 0xFD:
+        right();
+        Serial.println("right"); break;
+      case 0xFB:
+        left();
+        Serial.println("left");break; 
+      case 0xF7:
+        backward();
+        Serial.println("down");break;
+      case 0xEF:
+        forward();
+        Serial.println("center");break;
+      default :
+        Serial.println("unknow\n");
+    }
+    while(value != 0xFF)
+    {
+      PCF8574Write(0x1F | PCF8574Read());
+      value = PCF8574Read() | 0xE0;
+      delay(10);
+    }
+    stop();  
+    beep_off;
+  }
+}
+
+void PCF8574Write(byte data)
+{
+  Wire.beginTransmission(Addr);
+  Wire.write(data);
+  Wire.endTransmission(); 
+}
+
+byte PCF8574Read()
+{
+  int data = -1;
+  Wire.requestFrom(Addr, 1);
+  if(Wire.available()) {
+    data = Wire.read();
+  }
+  return data;
+}
+
+void forward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,LOW);  
+  digitalWrite(BIN2,HIGH); 
+}
+
+void backward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void right()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void left()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);  
+}
+
+void stop()
+{
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,LOW);  
+}
